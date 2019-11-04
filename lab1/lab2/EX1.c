325fdd75bbc79f6dd55c8abf6d4f304eed425ddf
guokeyu-0112 <2897868864@qq.com>
2019-11-04 02:02:52 -0800

    added a ex1.c file

---
+++ lab1/lab2/EX1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    unsighed int a=1,b=0;
+    while a>0
+    {
+        a++;
+    }
+    printf("the maximum number of unsighed in is:%u\n",a-1)'
+     unsighed int n=a-1;
+     do
+     {
+         n=n/10;
+         b++;
+     }while(n>0)
+     printf(there are %d digits in the maximum number of unsighed int,\n",b)
+     
+     return 0
+}
