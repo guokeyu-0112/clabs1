c1923a1a85197a9e89bb0d5af5c0408fa5eb6a56
guokeyu-0112 <2897868864@qq.com>
2019-11-04 05:32:54 -0800

    added a EX1.c file

--- lab1/lab2/EX1.c
+++ lab1/lab2/EX1.c
@@ -2,19 +2,21 @@
 
 int main()
 {
-    unsighed int a=1,b=0;
-    while a>0
-    {
-        a++;
-    }
-    printf("the maximum number of unsighed in is:%u\n",a-1)'
-     unsighed int n=a-1;
+    unsigned int a=1,b=0;
+    while (a>0)
+        a<<1;
+        ++a;
+    printf("the maximum number of unsigned in is:%u\n",a-1);
+      
+      unsigned int n=a-1;
      do
      {
          n=n/10;
          b++;
-     }while(n>0)
-     printf(there are %d digits in the maximum number of unsighed int,\n",b)
+     }
+     while(n>0)
+     printf("there are %d\n digits in the maximum number of unsigned int",b)
      
      return 0
+    
 }
